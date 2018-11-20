70df2b80dfaf7e066bbbdc3caf9c845620e5f0f9
niki_harshi <geenanik25@gmail.com>
2018-11-20 15:56:13 +0530

    Hurray! Can encrypt any no. of letters + implmented padding through file handling NO GUI

---
+++ gui_two_more.py
@@ -0,0 +1,204 @@
+from Tkinter import *
+import random
+import math
+
+letters={'a':26,'b':1,'c':2,'d':3,'e':4,'f':5,'g':6,'h':7,'i':8,'j':9,'k':10,'l':11,'m':12,'n':13,'o':14,'p':15,'q':16,'r':17,'s':18,'t':19,'u':20,'v':21,'w':22,'x':23,' ':0,'y':24,'z':25}
+
+numbers={}
+for letter,num in letters.items():
+    numbers[num]=letter
+   
+
+def exponentMod(A,B,C) :
+
+    # Base cases 
+    if (A == 0):
+        return 0
+    if (B == 0):
+        return 1 
+  
+    # If B is even 
+   
+    if (B % 2 == 0):
+        y = exponentMod(A, B / 2, C)
+        y = (y * y) % C 
+    
+  
+    # If B is odd 
+    else:
+        y = A % C
+        y = (y * exponentMod(A, B - 1, C) % C) % C 
+   
+    return (int)((y + C) % C) 
+
+def isPrime(n):
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return False
+    return True
+
+def gcd(a,b): 
+    if(b==0): 
+        return a 
+    else: 
+        return gcd(b,a%b)
+
+def gen_public_key(n,phi):
+	list_random_e=[i for i in range(1,phi) if (n%i!=0 and gcd(i,phi) ==1)]
+	e=random.choice(list_random_e)
+	return (n,e)
+
+def gen_private_key(n,phi,e):
+	list_random_k=[i for i in range(1,phi) if (((i*phi)+1)%e ==0)]
+	k=random.choice(list_random_k)
+	print "k=",k
+	d=((k*phi)+1)/e
+	return (n,d)
+
+f=open("toBeEncrypted.txt","r")
+list_of_strings=f.read().splitlines()
+z=list_of_strings[0]
+f.close()		
+
+primes=[i for i in range(26,1000) if isPrime(i)]
+p=random.choice(primes)
+primes.remove(p)
+q=random.choice(primes)
+print "p=",p
+print "q=",q
+
+n=p*q
+phi=(p-1)*(q-1)
+
+public_key=gen_public_key(n,phi)
+print "public key (n,e) : ",public_key
+private_key=gen_private_key(n,phi,public_key[1])
+print "private key (n,d) ",private_key
+
+
+def change(c):
+	d=""
+	while(c>0):
+		digit=c%10
+		e=random.randint(0,9)
+		d+=str(e)+str(digit)   #adding random then actual digit , on reversing we get actual digit followed by random no
+		c=c/10
+	return d[::-1]
+	
+
+
+def Encrypt():
+	for i in z:
+		c=exponentMod(letters[i],public_key[1],n)
+		print "each letter encrypted=",c
+		s=change(c)
+		print "each letter after padding: ",s
+		f=open("encrypted.txt","a+")
+		s=s+"\n"
+		f.write(s)
+		f.close()
+
+Encrypt()
+
+def changeback(i):
+	print i
+	original=""
+	for n in range(0,len(i)):
+		if(n%2==0):
+			original+=i[n]
+		
+	return int(original)
+
+def Decrypt():
+	f=open("encrypted.txt","r")
+	s=f.read().splitlines()
+	f.close()
+	for i in s:
+		cipher=changeback(i)
+		print "cipher :",cipher
+		decryp=exponentMod(cipher,private_key[1],n)
+		print "each letter decrypted=",decryp
+		dmsg=numbers[decryp]
+		print "The decrypted charcter is :",dmsg
+		f=open("Decrypted.txt","a+")
+		f.write(dmsg)
+	f.close()
+	
+	
+Decrypt()
+
+
+'''
+root = Tk()    
+# GUI title                
+root.title('RSA ENCRYPTOR AND DECRYPTOR')   
+
+# ******************plaintext input*****************
+l = Label(root,text='Input the plaintext')
+l.pack() 
+
+# input plaintext
+entryvalue = Entry(root) 
+entryvalue.pack()
+
+# click the Encrypt button
+button = Button(root,text="Encrypt",command=Encrypt) 
+button.pack()
+
+# show the ciphertext info.
+show = Label(root,text='Show Ciphertext:')
+show.pack() 
+listbox  = Listbox(root,height = 1, width = 40)        
+listbox.pack() 
+# ******************plaintext input ending*****************
+
+
+
+
+
+# ******************ciphertext input*****************
+label = Label(root,text='Input the ciphertext')
+label.pack() 
+
+# input ciphertext
+entryvalue2 = Entry(root) 
+entryvalue2.pack()
+
+# click the Decrypt button
+button2 = Button(root,text="Decrypt",command=Decrypt) 
+button2.pack()
+
+# show the plaintext info.
+show2 = Label(root,text='Show Plaintext:')
+show2.pack() 
+listbox2  = Listbox(root,height = 1, width = 40)         
+listbox2.pack() 
+# ******************ciphertext input ending*****************
+
+
+root.mainloop()                 
+'''
+
+
+
+	
+
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
